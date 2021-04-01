16906a1173e97172a4c54b374aed97bab88ff1fe
anritschel <arits2004@yahoo.com>
2021-04-01 12:24:25 -0500

    Added check for active directory module

--- move-inactive.ps1
+++ move-inactive.ps1
@@ -12,15 +12,22 @@ $smtpPort = 587
 $smtpCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $emailFrom, $PWord
 $body = "<html><p>The following users were disabled on $Today : </p>"
 
+if(Get-Module -ListAvailable -Name activedirectory){
+    Import-Module activedirectory
+}
+else{
+    Install-Module activedirectory
+    Import-Module activedirectory
+}
 
 Get-ADUser -Filter * -Properties "LastLogonDate" |ForEach-Object{
-If ($_.Enabled -eq 'True'){
-    Write-Host "Unmoved: $($_.name) They logged in on $($_.LastLogonDate)"
-    }
-Else{
+If ($_.LastLogonDate -lt $today.AddDays(-90){
     $inactive += $_.name
     Move-ADObject $_.ObjectGUID -TargetPath "OU=Inactive Users, $($domain.rootDomainNamingContext)"
     Set-ADUser $_.ObjectGUID -Enabled $false -Description "Acct disabled on $Today due to inactivity. User last logged in on $($_.LastLogonDate)"
+    }
+Else{
+    Write-Host "Unmoved: $($_.name) They logged in on $($_.LastLogonDate)"
     }
 }
 
