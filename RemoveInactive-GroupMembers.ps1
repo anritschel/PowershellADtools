07b2d1e7ee15456f200d4571de15efbeaa2a1388
anritschel <arits2004@yahoo.com>
2021-04-02 12:57:09 -0500

    add email to RemoveInactive-GroupMembers.ps1

--- RemoveInactive-GroupMembers.ps1
+++ RemoveInactive-GroupMembers.ps1
@@ -4,6 +4,17 @@ Param(
     [string]$group
 )
 
+$domain = get-adrootdse
+$inactive = @()
+$emailFrom = "Alerts@soratech.com"
+$emailTo = "Alerts@soratech.com"
+$hostName = hostname
+$subject = "Group membership for $($group) has changed on $($hostName) for domain $($domain.rootDomainNamingContext)"
+$smtpServer = "smtp.outlook.com"
+$smtpPort = 587
+$smtpCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $emailFrom, $PWord
+$body = "<html><p>The following users were disabled on $Today : </p>"
+
 #loads active directory PS module
 if(Get-Module -ListAvailable -Name activedirectory){
     Import-Module activedirectory
@@ -24,3 +35,10 @@ Get-ADGroupMember -Identity $group | ForEach-Object {
         }
     }
 }
+$inactive | foreach-object{
+    $body += "<p> $_</p>"
+}
+
+
+$body += "</html>"
+Send-MailMessage -From $emailFrom -to $emailTo -Subject $subject -body $body -SmtpServer $smtpServer -port $smtpPort -UseSsl -Credential $smtpCredentials -bodyashtml
