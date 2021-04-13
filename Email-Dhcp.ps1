bb47798f58cfc4a05876a7c056a072d7076c121c
Andrew Ritschel <arits2004@yahoo.com>
2021-04-12 20:55:52 -0500

    finalized Email-dhcp v1

--- Email-Dhcp.ps1
+++ Email-Dhcp.ps1
@@ -13,19 +13,19 @@ $EmailMessage = "<style>
 			border:1px solid #000;
 		}
 	</style>
-<p>The following DHCP Scopes were found on  $($env:COMPUTERNAME), add to the appropriate company's information</p><table>"
+<p>The following DHCP Scopes were found on  $($env:COMPUTERNAME), add to the appropriate company's information</p>"
 
 $scopes = Get-DhcpServerv4Scope 
 
 $scopes | ForEach-Object {
     $DhcpObject = [PSCustomObject]@{
 		ScopeID = $_.ScopeId
-		Router = Get-DhcpServerv4OptionValue -ScopeID $_.ScopeID -OptionId 3
-		DnsServers = Get-DhcpServerv4OptionValue -ScopeID $_.ScopeID -OptionId 6
+		Router = (Get-DhcpServerv4OptionValue -ScopeID $_.ScopeID -OptionId 3).Value
+		DnsServers = (Get-DhcpServerv4OptionValue -ScopeID $_.ScopeID -OptionId 6).Value
     }
-    $EmailMessage += "<tr><td> Scope ID</td><td> $($DhcpObject.ScopeID)</td></tr><tr><td> Router </td><td> $($DhcpObject.Router)</td></tr><tr><td> DNS Servers</td><td> $($DhcpObject.DnsServers)</td></tr></table><table> "
+    $EmailMessage += "<p>Scope Options:</p><table><tr><td> Scope ID</td><td> $($DhcpObject.ScopeID)</td></tr><tr><td>Range</td><td>$($_.StartRange) - $($_.EndRange)</td></tr><tr><td> Router </td><td> $($DhcpObject.Router)</td></tr><tr><td> DNS Servers</td><td> $($DhcpObject.DnsServers)</td></tr></table><p>Address Reservations:</p><table><tr><td>Name</td><td>Mac Address</td><td>IP address</td><td>Description</td></tr>"
     Get-DhcpServerV4Reservation -ScopeId $_.ScopeId | ForEach-Object { 
-        $EmailMessage += "<tr><td>Name</td><td>Mac Address</td><td>IP address</td><td>Description</td></tr><tr><td>$($_.Name)</td><td>$($_.ClientId)</td><td>$($IPAddress)</td><td>$($_.Description)</td></tr>"
+        $EmailMessage += "<tr><td>$($_.Name)</td><td>$($_.ClientId)</td><td>$($_.IPAddress)</td><td>$($_.Description)</td></tr>"
     } 
     $EmailMessage += "</table>"
 
